b800e56f980f44292f0a73adf38fd2c57711578b
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-03-11 14:00:57 -0500

    image required in the markup

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -54,6 +54,7 @@ export default function CreateProduct() {
       <label htmlFor="name">
           Image
           <input
+            required
             type="file"
             id="image"
             name="image"
