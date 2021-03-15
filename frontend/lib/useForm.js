74d2e24fc0c4aa57d13fcbd77caec618d3586384
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-03-15 16:10:41 -0400

    using useEffect to deal with tricking loading state issue

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function useForm(initial = {}) {
     const [inputs, setInputs] = useState(initial)
+    const initialValues = Object.values(initial).join('')
+
+    useEffect(() => {
+        // This function runs when the things we are watching change
+        setInputs(initial)
+    }, [initialValues])
 
     function handleChange(e) {
         let { value, name, type } = e.target
