9beb492e237ce8d918a5a9a2de1022b777d5273a
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-03-16 13:53:29 -0400

    pagination links

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,9 +1,12 @@
+import Pagination from "../components/Pagination";
 import Products from "../components/Products";
 
 export default function ProductsPage() {
     return (
         <>
+            <Pagination page={1} />
             <Products/>
+            <Pagination page={1} />
         </>
     )
 }
