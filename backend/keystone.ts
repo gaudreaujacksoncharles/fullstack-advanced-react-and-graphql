58e752018980684be945a899fa6bfdca9fd2d1ef
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-03-19 15:03:59 -0400

    password reset: sending the email

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -9,6 +9,9 @@ import { Product } from './schemas/Product';
 import { User } from './schemas/User';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
+
+function check(name: string) { }
 
 const databaseURL =
     process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -28,7 +31,8 @@ const { withAuth } = createAuth({
     },
     passwordResetLink: {
         async sendToken(args) {
-            console.log(args);
+            // send the email
+            await sendPasswordResetEmail(args.token, args.identity);
         },
     },
 });
