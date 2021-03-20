db13163b68ba5c01a1356a0d520a9bad2c447134
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-03-20 18:49:32 -0400

    default styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -41,7 +41,8 @@ const GlobalStyles = createGlobalStyle`
     text-decoration: underline;
   }
   button {
-    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue';
+    fong-weight:700;
   }
   h1, h2, h3, h4, h5, h6 {
     margin-top:0;
