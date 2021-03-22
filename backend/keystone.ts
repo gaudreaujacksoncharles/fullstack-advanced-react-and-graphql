6c8378e2cc3b5b542600809f0faaf5647cb9e84e
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-03-22 11:42:32 -0400

    cart: creating the data type and two way relationships

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -7,6 +7,7 @@ import {
 import { ProductImage } from './schemas/ProductImage';
 import { Product } from './schemas/Product';
 import { User } from './schemas/User';
+import { CartItem } from './schemas/CartItem';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
@@ -61,6 +62,7 @@ export default withAuth(
             User,
             Product,
             ProductImage,
+            CartItem,
         }),
         ui: {
             // Show the UI only for poeple who pass this test
