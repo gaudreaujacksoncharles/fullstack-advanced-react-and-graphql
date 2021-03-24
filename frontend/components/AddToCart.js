a73671409c9b86e50a0c81d64d453d296b36e64e
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-03-24 16:04:04 -0400

    open cart when adding to cart

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import { useMutation } from "@apollo/client"
 import gql from "graphql-tag"
 import { CURRENT_USER_QUERY } from "./User"
+import { useCart } from '../lib/cartState'
 
 const ADD_TO_CART_MUTATION = gql`
     mutation ADD_TO_CART_MUTATION($id: ID!) {
@@ -15,8 +16,14 @@ export default function AddToCart({ id }) {
         variables: { id: id },
         refetchQueries: [{ query: CURRENT_USER_QUERY }]
     })
+    const { openCart } = useCart()
+    function handleClick(id) {
+        addToCart(id)
+        openCart()
+    }
+
     return (
-        <button disabled={loading} type='button' onClick={addToCart}>
+        <button disabled={loading} type='button' onClick={handleClick}>
             Add{loading && 'ing'} to cart
         </button>
     )
