8cb6bb51b1c062026cf9988d1a9b477ce740adc6
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-04-12 14:01:29 -0400

    linking up our frontend to the custom backend checkout mutation

--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -60,6 +60,7 @@ async function checkout(
         console.log(err)
         throw new Error(err.message)
     })
+    console.log(charge)
     // 4. Convert the cartItems to orderItems
 
     // 5. Create the order and return it
--- frontend/components/Checkout/index.js
+++ frontend/components/Checkout/index.js
@@ -3,6 +3,22 @@ import { CardElement, Elements, useElements, useStripe } from '@stripe/react-str
 import { CheckoutFormStyles, CheckoutButton } from './styles'
 import { useState } from 'react';
 import nProgress from 'nprogress';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+const CREATE_ORDER_MUTATION = gql`
+    mutation CREATE_ORDER_MUTATION($token: String!) {
+        checkout(token: $token) {
+            id
+            charge
+            total
+            items {
+                id
+                name
+            }
+        }
+    }
+`
 
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
@@ -11,6 +27,7 @@ function CheckoutForm() {
     const [loading, setLoading] = useState(false)
     const stripe = useStripe(false)
     const elements = useElements()
+    const [checkout, { error: graphQLError }] = useMutation(CREATE_ORDER_MUTATION)
 
     async function handleSubmit(e) {
         // 1. Stop the form from submiting and turn the loader on
@@ -28,11 +45,18 @@ function CheckoutForm() {
         // 4. Handle any errors from stripe
         if (error) {
             setError(error)
+            nProgress.done()
+            return
         }
         // 5. Send the token from step 3 to our keystone server, via a custom mutation
-
+        const order = await checkout({
+            variables: {
+                token: paymentMethod.id
+            }
+        })
+        console.log('Finished with the oder')
         // 6. Change the page to view the order
-
+        console.log('order', order)
         // 7. Cose the cart
 
         // 8. Turn the loader off
@@ -42,6 +66,7 @@ function CheckoutForm() {
     return (
         <CheckoutFormStyles onSubmit={handleSubmit}>
             { error && <p>{error.message}</p> }
+            { graphQLError && <p>{graphQLError.message}</p> }
             <CardElement />
             <CheckoutButton>Check Out Now</CheckoutButton>
         </CheckoutFormStyles>
