37cae79ef92a2862be9607ec5b19145b5a983648
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-04-12 16:44:55 -0400

    user and field based permissions

--- backend/access.ts
+++ backend/access.ts
@@ -27,6 +27,9 @@ export const permissions = {
 // Rules can return a boolean - yes or no - or a filter which limits which products they can CRUD.
 export const rules = {
     canManageProducts({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
         // 1. Do they have the permission of canManageProducts
         if (permissions.canManageProducts({ session })) {
             return true;
@@ -35,6 +38,9 @@ export const rules = {
         return { user: { id: session.itemId } };
     },
     canOrder({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
         // 1. Do they have the permission of canManageProducts
         if (permissions.canManageCart({ session })) {
             return true;
@@ -43,6 +49,9 @@ export const rules = {
         return { user: { id: session.itemId } };
     },
     canManageOrderItems({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
         // 1. Do they have the permission of canManageProducts
         if (permissions.canManageCart({ session })) {
             return true;
@@ -51,10 +60,23 @@ export const rules = {
         return { order: { user: { id: session.itemId } } };
     },
     canReadProducts({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
         if (permissions.canManageProducts({ session })) {
             return true; // They can read everything!
         }
         // They should only see available products (based on the status field)
         return { status: 'AVAILABLE' };
+    },
+    canManageUsers({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
+        if (permissions.canManageUsers({ session })) {
+            return true;
+        }
+        // Otherwise they may only update themselves!
+        return { id: session.itemId };
     },
 };
