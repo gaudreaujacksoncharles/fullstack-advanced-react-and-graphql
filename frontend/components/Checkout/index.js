33948ed0099e82f5e7f4acc5e310833e25b87d9f
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-04-12 12:29:56 -0400

    writing our client side checkout handler logic

--- frontend/components/Checkout/index.js
+++ frontend/components/Checkout/index.js
@@ -1,32 +1,59 @@
-import styled from 'styled-components';
 import { loadStripe } from '@stripe/stripe-js';
-import { CardElement, Elements } from '@stripe/react-stripe-js';
-
-const CheckoutFormStyles = styled.form`
-  box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
-  border: 1px solid rgba(0, 0, 0, 0.06);
-  border-radius: 5px;
-  padding: 1rem;
-  display: grid;
-  grid-gap: 1rem;
-`;
+import { CardElement, Elements, useElements, useStripe } from '@stripe/react-stripe-js';
+import { CheckoutFormStyles, CheckoutButton } from './styles'
+import { useState } from 'react';
+import nProgress from 'nprogress';
 
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
-function Checkout() {
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log('We gotta do some work..');
-  }
+function CheckoutForm() {
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState(false)
+    const stripe = useStripe(false)
+    const elements = useElements()
 
-  return (
-    <Elements stripe={stripeLib}>
-      <CheckoutFormStyles onSubmit={handleSubmit}>
-        <CardElement />
-        <button>Check Out Now</button>
-      </CheckoutFormStyles>
-    </Elements>
-  );
+    async function handleSubmit(e) {
+        // 1. Stop the form from submiting and turn the loader on
+        e.preventDefault();
+        setLoading(true)
+        console.log('We gotta do some work..');
+        // 2. Start the page transition
+        nProgress.start()
+        // 3. Create the payment method via stripe (token comes back here if successful)
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card: elements.getElement(CardElement)
+        })
+        console.log(paymentMethod)
+        // 4. Handle any errors from stripe
+        if (error) {
+            setError(error)
+        }
+        // 5. Send the token from step 3 to our keystone server, via a custom mutation
+
+        // 6. Change the page to view the order
+
+        // 7. Cose the cart
+
+        // 8. Turn the loader off
+        setLoading(false)
+        nProgress.done()
+    }
+    return (
+        <CheckoutFormStyles onSubmit={handleSubmit}>
+            { error && <p>{error.message}</p> }
+            <CardElement />
+            <CheckoutButton>Check Out Now</CheckoutButton>
+        </CheckoutFormStyles>
+    );
+}
+
+function Checkout() {
+    return (
+        <Elements stripe={stripeLib}>
+            <CheckoutForm/>
+        </Elements>
+    )
 }
 
 export { Checkout };
