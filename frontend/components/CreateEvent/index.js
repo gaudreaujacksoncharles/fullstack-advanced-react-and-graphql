d54cd5cd153d333c4c2f31fa35d99997c86776da
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-04-17 01:17:32 -0400

    functionning form with mutation

--- frontend/components/CreateEvent/index.js
+++ frontend/components/CreateEvent/index.js
@@ -1,21 +1,55 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
 import { useCreateEvent } from "../../lib/createEventState";
+import useForm from "../../lib/useForm";
 import { CreateEventCenter, CreateEventFooter, CreateEventHeader, CreateEventPanel, CreateEventGeneral, CreateEventStyles, CreateEventInput, CreateEventButton, CreateEventButtonGrayLabel, CreateEventButtonGreenLabel, CreateEventButtonLabel, CreateEventTextarea } from "./styles";
 
+const CREATE_EVENT_MUTATION = gql`
+    mutation CREATE_EVENT_MUTATION(
+        $title: String!
+        $description: String
+    ) {
+        createEvent(data: {
+            title:$title
+            description:$description
+        }) {
+            id
+            title
+            description
+        }
+    }
+`
+
 export default function CreateEvent() {
+    const { inputs, handleChange } = useForm()
+    const [ createProduct, { loading, error, data } ] = useMutation(CREATE_EVENT_MUTATION, {
+        variables: inputs
+    })
     const { createEventOpen, closeCreateEvent } = useCreateEvent()
     if (!createEventOpen) return null
     return (
         <CreateEventStyles>
-            <CreateEventPanel>
+            <CreateEventPanel
+                onSubmit={async (e) => {
+                    e.preventDefault()
+                    const res = await createProduct()
+                }}>
                 <CreateEventGeneral>
                     <CreateEventHeader>
                         <CreateEventInput
+                            required
+                            name='title'
                             placeholder='Title'
+                            onChange={handleChange}
+                            value={inputs.title}
                         />
                     </CreateEventHeader>                    
                     <CreateEventCenter>
                         <CreateEventTextarea
+                            name='description'
                             placeholder='Description'
+                            onChange={handleChange}
+                            value={inputs.description}
                         />
                     </CreateEventCenter>
                     <CreateEventFooter>
@@ -24,7 +58,7 @@ export default function CreateEvent() {
                                 Cancel
                             </CreateEventButtonGrayLabel>  
                         </CreateEventButton>
-                        <CreateEventButton>
+                        <CreateEventButton type='submit'>
                             <CreateEventButtonGreenLabel>
                                 Create
                             </CreateEventButtonGreenLabel>  
