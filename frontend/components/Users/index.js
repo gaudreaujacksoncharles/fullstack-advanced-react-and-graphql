f57c1094d3ff51365f9c6a05964df561cba5e28a
Charles Gaudreau Jackson <charles.gaudreau.jackson.creation@gmail.com>
2021-05-03 00:56:28 -0400

    users/

--- frontend/components/Users/index.js
+++ frontend/components/Users/index.js
@@ -1,7 +1,30 @@
+import { useQuery } from "@apollo/client"
+import gql from "graphql-tag"
+
+export const ALL_USERS_QUERY = gql`
+    query ALL_USERS_QUERY {
+        allUsers(skip:0) {
+            id
+            name
+            events {
+                title
+            }
+        }
+    }
+`
+
 export default function Users() {
+    const { data, loading, error } = useQuery(ALL_USERS_QUERY)
+    console.log(data)
     return (
         <>
-            
+            {data?.allUsers.map(user => {
+                return (
+                    <>
+                        {user.name}
+                    </>
+                )
+            })}
         </>
     )
 }
